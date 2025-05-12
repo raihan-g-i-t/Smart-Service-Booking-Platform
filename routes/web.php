258c91bf564df6bcf770dbfd46b8d91c81615656
raihan-g-i-t <opu.raihankhan@gmail.com>
2025-05-12 19:48:45 +0600

    12 May

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,4 @@
 use App\Http\Controllers\Admin\AdminLoginController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::get('/login', [AdminLoginController::class, 'login'])->name('admin.login');
