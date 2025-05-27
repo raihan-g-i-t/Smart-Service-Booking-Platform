c860de383bcc13b3568b3874832f64e5d6318800
raihan-g-i-t <opu.raihankhan@gmail.com>
2025-05-27 20:27:18 +0600

    27 May

--- app/Http/Requests/UserLoginRequest.php
+++ app/Http/Requests/UserLoginRequest.php
@@ -22,7 +22,8 @@ class UserLoginRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'email' => 'required',
+            'password' => 'required|email'
         ];
     }
 }
