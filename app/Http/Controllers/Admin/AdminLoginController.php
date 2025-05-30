4ee93319df88a15f85712debf2871bd1b06f1faa
raihan-g-i-t <opu.raihankhan@gmail.com>
2025-05-30 19:56:42 +0600

    30 May

--- app/Http/Controllers/Admin/AdminLoginController.php
+++ app/Http/Controllers/Admin/AdminLoginController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AdminLoginController extends Controller
@@ -12,6 +13,7 @@ class AdminLoginController extends Controller
     }
 
     public function loginProcess(Request $request){
-        return view('admin.dashboard');
+        $user = count(User::all());
+        return view('admin.dashboard', ['number' => $user]);
     }
 }
